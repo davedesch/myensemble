0a61ac45b563c44e73a1fab8abdf06f61cdde6c8
Dave Descheneaux <davedesch@gmail.com>
2015-03-12 14:09:51 -0700

    serve static assets true

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Myensemble
   class Application < Rails::Application
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
