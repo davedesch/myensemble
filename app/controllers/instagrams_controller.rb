77540ee1c44b4b52721d034f357291d9c1659962
immber <jessiekrushing@hotmail.com>
2015-03-18 18:10:41 -0700

    adds helpers and controllers

--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,9 +1,9 @@
 class InstagramsController < ApplicationController
 
 
-  CALLBACK_URL = "https://floating-retreat-7259.herokuapp.com/instagram/oauth/callback"
+  CALLBACK_URL = "http://localhost:3000/instagram/oauth/callback"
 
- CLIENT = ENV['CLIENT_ID']
+  CLIENT = ENV['CLIENT_ID']
 
   #handles 'oauth/connect'
   def login
@@ -42,51 +42,10 @@ private
         newuser.auth_token = response.access_token
         newuser.instagram_name = response.user.full_name
       end
+      user.auth_token = response.access_token
+      user.save
     return user
   end
 
 
 end
-#   CLIENT = ENV['CLIENT_ID']
-
-#   #handles 'oauth/connect'
-#   def login
-#     redirect_to "https://api.instagram.com/oauth/authorize/?client_id=#{CLIENT}&redirect_uri=#{CALLBACK_URL}&response_type=code"
-#   end
-
-#   #handles 'oauth/callback'
-#   def authorized
-#     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-#     user = create_or_login(response)
-#     session[:user_id] = user.id
-#     redirect_to user_path(user.id)
-#   end
-
-#   def recent
-#     current_user = User.find(session[:user_id])
-#     client = Instagram.client(access_token: current_user.auth_token)
-#     user = client.user
-#     @images = []
-#     client.user_recent_media.each do |media|
-#       p media.images
-#       @images.push(media.images.standard_resolution.url)
-#     end
-#     render file: '/app/views/users/itworked', layout: false
-#   end
-
-
-# private
-
-#   def create_or_login(response)
-#    user = User.where(username: response.user.username).first_or_create do |newuser|
-#         newuser.username = response.user.username
-#         newuser.password = response.access_token
-#         newuser.avatar = response.user.profile_picture
-#         newuser.auth_token = response.access_token
-#         newuser.instagram_name = response.user.full_name
-#       end
-#     return user
-#   end
-
-
-# end
--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -4,23 +4,54 @@ class OutfitsController < ApplicationController
 
   def index
     user = User.find(params[:user_id])
-    p user
     outfits = Outfit.where(user_id: user.id).order('created_at DESC').limit(10)
     results = []
     outfits.each do |outfit|
-      results.push({title: outfit.title, image: outfit.image_url, avg_rating: outfit.average_ratings, popularity: outfit.popularity, created_at: outfit.created_at})
+      results.push({outfit_id: outfit.id, title: outfit.title, image: outfit.image_url, avg_rating: outfit.average_ratings, popularity: outfit.popularity, created_at: outfit.created_at})
     end
     render json: results
   end
 
   def new
+    @current_user = User.find(params[:user_id])
+    @outfit = Outfit.new
+    5.times do
+      article = @outfit.articles.build
+    end
     render :new, layout: false
   end
 
   def create
     user = User.find(params[:user_id])
-    @dick = user.outfits.create(outfit_params)
+    @outfit = user.outfits.create(outfit_params)
+    articles = params[:outfit][:articles_attributes]
+    if articles
+      articles.each do |key, value|
+         @outfit.articles.create(value)
+      end
+    end
     redirect_to user_path(user)
+  end
+
+  def edit
+    @outfit = Outfit.find(params[:id])
+    render :edit, layout: false
+  end
+
+  def update
+    user = User.find(params[:user_id])
+    outfit = Outfit.find(params[:id])
+    outfit.attributes=(outfit_params)
+    outfit.save
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    outfit = Outfit.find(params[:id])
+    outfit.destroy
+
+    redirect_to "/users/#{user.id}"
   end
 
   private
@@ -28,5 +59,6 @@ class OutfitsController < ApplicationController
   def outfit_params
     params.require(:outfit).permit(:image_url, :user_id, :title, :caption, :gender, :commit)
   end
+
 
 end
